1e96355cc336f4f1e22ca75a2184b60cb41eddb0
scarletnguyen <scarlet.nguyen01@gmail.com>
2020-06-25 20:01:26 -0400

    Organize folders

--- scrape/combine.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-a = pd.read_csv("shortened_text_only.csv")
-b = pd.read_csv("scraped_text.csv")
-# b = b.dropna(axis=1)
-merged = a.merge(b, on='Ad_ID')
-merged.dropna(thresh=3)
-merged.to_csv("output.csv", index=False)
