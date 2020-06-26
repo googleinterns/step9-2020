53710a4b265108f35debe63c27d79e8a80ce35b7
Rob Marcus <robmarcus@google.com>
2020-06-26 11:50:50 -0400

    Analyzed CSVs, file system cleanup, api key removed

---
+++ preprocess/analysis/mapping.py
@@ -0,0 +1,49 @@
+"""
+@Author: Scarlet Nguyen 
+"""
+
+
+import csv
+
+from language_analysis import analyze_entities, analyze_sentiment
+
+def get_analysis(text):
+  return {
+    'sentiment': analyze_sentiment(text),
+    'entities': analyze_entities(text)
+  }
+
+file = open('input.csv')
+
+csv_file = csv.reader(file)
+
+count = 1
+
+existed = dict()
+
+for row in csv_file:
+  print(count)
+  if count > 36730:
+    analysis_info = []
+    if count == 1:
+      row.extend(['Headline_Sentiment', 'Headline_Entities', 'Content_Sentiment', 'Content_Entities'])
+    else:
+      headlineAndContent = [row[len(row) - 3], row[len(row) - 1]]
+      for text in headlineAndContent:
+        if text in existed:
+          analysis_info.append(existed[text].get('sentiment'))
+          analysis_info.append(existed[text].get('entities'))
+        else:
+          new_dict = get_analysis(text)
+          analysis_info.append(new_dict.get('sentiment'))
+          analysis_info.append(new_dict.get('entities'))
+          existed[text] = new_dict
+
+      row.extend(analysis_info)
+  
+    with open('output.csv','a') as csv_text:
+      csvwriter = csv.writer(csv_text)
+      csvwriter.writerow(row)
+      print(analysis_info)
+      print('\n')
+  count+=1
--- scrape/cloud language analysis/readme.txt
+++
@@ -1,6 +0,0 @@
-To run sentiment analysis on your local machine you will to follow these instructions:
-https://cloud.google.com/natural-language/docs/setup
-
-in particular, you will need an api key. Don't push your api key to github please. 
-
-The test file with STRING_ONE and STRING_TWO runs for me in about 1 second, so expect about 1/4 of a second per api call. Query rate limit is 600/minute so that's pretty good.  
