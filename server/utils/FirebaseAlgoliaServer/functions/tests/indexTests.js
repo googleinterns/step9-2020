f638d737833a99a847aaa19206f8a9c10ffb548d
Rob <robbie.marcus@gmail.com>
2020-07-01 04:15:06 +0000

    Working tests

--- server/utils/FirebaseAlgoliaServer/functions/tests/indexTests.js
+++ server/utils/FirebaseAlgoliaServer/functions/tests/indexTests.js
@@ -17,46 +17,130 @@
  * Experimentally determined times so that calls get processed correctly 
  *
  * TL;DR: 
- * - Can't test normally, so test it this abnormal way
+ * - Can't test normally, so test it this abnormal and _painful_ way
  * - Kind of stupid
  * - If it's stupid but it works...  
  */
 
 /*
  * Tests being performed:
- * - Ad something to firebase, make sure it's added to algolia.
- * - The above, but delete something and make sure it gets deleted from algolia.
+ * singleEntityDeliveredAndDeleted_SUCCESS
+ * - Add something to firebase, make sure it's added to algolia.
+ *   Delete the comment and make sure it gets deleted from algolia.
+ * singleEntityUpdated_SUCCESS
  * - The above, but also update in firebase and make sure update is propogated.
+ * twoEntitiesAddedDeletedIndependently_SUCCESS
  * - Add two things, make sure both get in.
- * - Add two things, delete one and make sure the other is still there.
+ *                   delete one and make sure the other is still there.
+ * twoEntitiesUpdateIndependently_SUCCESS
  * - Add two things, update one, make sure the other isn't modified. 
+ * RUN TESTS INDIVIDUALLY TO AVOID ASYNC ISSUES!
  */
 
 var helpers = require('./testHelpers');
 
-function test1() {
+
+/**
+ * Add an entity in firestore/algolia
+ * Then delete it from both DBs.
+ */
+function singleEntityDeliveredAndDeleted_SUCCESS() {
+  console.log("Testing singleEntityDeliveredAndDeleted_SUCCESS")
+
+  // Create in firestore and make sure it gets to algolia
+  var addPromise = 
+      helpers.addEntity({"Testing" : "if firebase entities reach algolia"});
+  var theyEqualPromise = 
+      helpers.checkAlgoliaObjectEqualsFirestoreEntityFromPromise(addPromise);
   
+  // Delete from firestore + algolia
+  // This will also validate that the entities actually got deleted
+  helpers.deleteFromEverywhere(addPromise);
+}
+
+/**
+ * Create an entity, update it, make sure the algolia version is updated
+ * NOTE: may have problems depending on how long it takes algolia to get update
+ *       easiest way to test this is through the firebase/algolia console. 
+ */
+function singleEntityUpdated_SUCCESS() {
+  console.log("Testing singleEntityUpdated_SUCCESS")
+
+  // Create in firestore and make sure it gets to algolia
+  var addPromise = 
+      helpers.addEntity({"Testing" : "if firebase entities reach algolia"});
+  var theyEqualPromise = 
+      helpers.checkAlgoliaObjectEqualsFirestoreEntityFromPromise(addPromise);
+
+  // setTimeout basically preserves the order
+  // otherwise delete could happen before update, for example. 
+  setTimeout(function(){
+    console.log("In the process of updating the entity")
+    var updatePromise = 
+        helpers
+        .updateEntityFromPromise(addPromise, {"Testing" : "if updates reach"});
+    var theyStillEqualPromise = 
+        helpers
+        .checkAlgoliaObjectEqualsFirestoreEntityFromPromise(addPromise);
+  }, 3000);  
+
+  helpers.deleteFromEverywhere(addPromise);
+}
+
+/**
+ * Creating two entities and deleting one of them shouldn't delete the other.
+ */
+function twoEntitiesAddedDeletedIndependently_SUCCESS() {
+  console.log("Testing twoEntitiesAddedDeletedIndependently_SUCCESS");
+
+  // Create in firestore and make sure it gets to algolia
+  var addPromiseOne = 
+        helpers.addEntity({"Testing" : "entity one"});
+  var addPromiseTwo = 
+        helpers.addEntity({"Testing" : "entity two"});
+  
+  var checkPromiseOneAdded = 
+      helpers.checkAlgoliaObjectEqualsFirestoreEntityFromPromise(addPromiseOne);
+  var checkPromiseTwoAdded = 
+      helpers.checkAlgoliaObjectEqualsFirestoreEntityFromPromise(addPromiseTwo);
+  
+  // Delete the first entity, make sure the other still exists/unchanged
+  helpers.deleteFromEverywhere(addPromiseOne);
+  var checkPromiseOneDeleted = 
+      helpers.checkObjectDeletedFromAlgoliaWithPromise(addPromiseOne);
+  var checkPromiseTwoUnchanged = 
+      helpers.checkAlgoliaObjectEqualsFirestoreEntityFromPromise(addPromiseTwo);
+
+  helpers.deleteFromEverywhere(addPromiseTwo);
 }
 
 
-async function test1() {
-  var addPromise = helpers.addEntity({"Testing" : "this out"});
-  console.log("Seeing if initially equal");
-  var theyEqualPromise = helpers.checkAlgoliaObjectEqualsFirestoreEntityFromPromise(addPromise);
-    
-  await helpers.sleep(5000);
-  console.log("Updating some stuff");
+/**
+ * Creating two entities and updating one of them shouldn't change 
+ * the state of the other entity.
+ */
+function twoEntitiesUpdateIndependently_SUCCESS() {
+  console.log("Testing twoEntitiesUpdateIndependently_SUCCESS");
 
-  setTimeout(function(){
-    var updatePromise = helpers.updateEntityFromPromise(addPromise, {"Testing" : "me"});
-    var theyStillEqualPromise = helpers.checkAlgoliaObjectEqualsFirestoreEntityFromPromise(updatePromise);
-  }, 3000);
+  var addPromiseOne = 
+        helpers.addEntity({"Testing" : "entity one"});
+  var addPromiseTwo = 
+        helpers.addEntity({"Testing" : "entity two"});
   
+  // setTimeout basically preserves the order
+  // otherwise delete could happen before update, for example.   
   setTimeout(function(){
-    var deletePromise = helpers.deleteEntityFromPromise(addPromise);
-    var checkDeleted = helpers.checkDeletedFromPromise(addPromise);
-    var checkDeletedFromAlgolia = helpers.checkObjectDeletedFromAlgoliaWithPromise(addPromise)
-  }, 10000);
+    console.log("In the process of updating the entity")
+    var updatePromise = 
+        helpers
+        .updateEntityFromPromise
+        (addPromiseOne, {"Testing" : "if updates reach"});
+    var twoIndependentFromUpdatePromise = 
+        helpers
+        .checkAlgoliaObjectEqualsFirestoreEntityFromPromise(addPromiseTwo);
+  }, 3000);  
 }
 
-test1();
+
+
+singleEntityDeliveredAndDeleted_SUCCESS();
--- server/utils/FirebaseAlgoliaServer/functions/tests/testHelpers.js
+++ server/utils/FirebaseAlgoliaServer/functions/tests/testHelpers.js
@@ -1,6 +1,5 @@
-/**
- * Initialize constants, libraries
- */
+// Initialize constants, libraries
+// These are public values 
 const config = {
                   apiKey: "AIzaSyCR9KXK2iDuaZ3n6WNzZnBORbtpgWE7dvI",
                   authDomain: "step9-2020-capstone.firebaseapp.com",
@@ -11,7 +10,7 @@ const admin = require('firebase-admin');
 admin.initializeApp(config);
 
 const db = admin.firestore();
-const COLLECTION = 'ads';
+const COLLECTION = 'ads'; 
 
 const functions = require('firebase-functions');
 const algoliasearch = require('algoliasearch');
@@ -21,16 +20,16 @@ const ADMIN_KEY = functions.config().algolia.key;
 
 // initialize algoliasearch API
 const client = algoliasearch(APP_ID, ADMIN_KEY);
-const index = client.initIndex('dev_ADS');
+const index = client.initIndex('dev_ADS'); // current index
 
 const DEFAULT_SLEEP = 5000; // Firebase->Algolia takes a couple secs, WCS
 
-/**
- * Firebase helper functions
- */
+// Firebase helper functions 
+
 /**
  * Add a json entity to COLLECTION 
- * Return a promise that, if fulfilled, contains the entity's firestore id
+ * @param {json=} jsonEntity the entity to be added to firestore
+ * @return {!Promise}  
  */
 async function addEntity(jsonEntity) {
   const entityPromise = await db.collection(COLLECTION).add(jsonEntity);
@@ -42,6 +41,8 @@ async function addEntity(jsonEntity) {
 
 /**
  * Delete an entity by it's corresponding firestore id
+ * @param {number=} entityId an entities id, taken directly from a promise
+ * @return {!Promise}
  */
 async function deleteEntity(entityId) {
   const deletePromise = await db.collection(COLLECTION).doc(entityId).delete()
@@ -51,7 +52,9 @@ async function deleteEntity(entityId) {
 
 /**
  * Update an entities field(s) by it's corresponding firestore id
- * Fields entered as a json object 
+ * @param {number=} entityId firestore id for entity being updated
+ * @param {json=} jsonEntityUpdates new values for entity k:v pairings
+ * @return {!Promise} 
  */
 async function updateEntity(entityId, jsonEntityUpdates) {
   const updatePromise = await db.collection(COLLECTION)
@@ -60,34 +63,48 @@ async function updateEntity(entityId, jsonEntityUpdates) {
   return updatePromise;
 }
 
-async function checkEntityMatches(entityId, expectedData) {
+/**
+ * Checks that the current DB entry Equals the expected data
+ * If the DB entry doesn't exist, throws an error
+ * If the DB entry insn't equal, throws a different error
+ * If successful, writes to the console
+ * @param {number=} entityId firestore id for entity being updated
+ * @param {json=} expectedData expected DB entry
+ * @return {!Promise} 
+ */
+async function checkEntityEquals(entityId, expectedData) {
   const entity = await db.collection(COLLECTION).doc(entityId).get();
   if (!entity.exists) {
-    throw(`No such document found for ${entityId}`);
+    throw(`Document ${entityId} firestore retrieval FAILURE`);
   } else {
     const entityData = entity.data()
     if (!isEquivalent(expectedData, entityData)){
       throw(`\nExpected ${JSON.stringify(expectedData)}\nGot ${JSON.stringify(entityData)}`); 
     }
-    console.log(`Expected ${JSON.stringify(expectedData)}\nGot ${JSON.stringify(entityData)}\nLGTM!`)
-    return true;
-  }
-}
-
-async function checkDeleted(entityId) {
-  const entity = await db.collection(COLLECTION).doc(entityId).get();
-  if(entity.exists) {
-    throw(`Document ${entityId} never got deleted.`)
-  } else {
-    console.log(`Document ${entityId} succesfully deleted.`)
+    console.log(`Expected ${JSON.stringify(expectedData)}\nGot ${JSON.stringify(entityData)}\nSUCCESS!`)
     return true;
   }
 }
 
 /**
+ * Validates that an entity has been deleted from firestore
+ * If it still exists, throws an error
+ * If it's been deleted, writes to the console
+ * @param {number=} entityId firestore id for entity being updated
+ */
+async function checkDeleted(entityId) {
+  const entity = await db.collection(COLLECTION).doc(entityId).get();
+  if(entity.exists) {
+    throw(`Document ${entityId} delete FAILURE.`)
+  } else {
+    console.log(`Document ${entityId} delete SUCCESS.`)
+  }
+}
+
+/**
  * Wrapper function for updateEntity
- * entityPromise does not need to be a QuerablePromise
- * Will throw a string exception if promise not fulfilled yet 
+ * @param {!Promise=} entityPromise promise returned when entity was made
+ * @param {json=} jsonEntityUpdates new values for entity k:v pairings
  */
 function updateEntityFromPromise(entityPromise, jsonEntityUpdates) {
   var updatePromise = 
@@ -99,8 +116,9 @@ function updateEntityFromPromise(entityPromise, jsonEntityUpdates) {
 
 /**
  * Wrapper function for deleteEntity
- * entityPromise does not need to be a QuerablePromise
- * Will throw a string exception if promise not fulfilled yet 
+ * Node JS requires a catch statement 
+ * @param {!Promise=} entityPromise promise returned when entity was made
+ * @return {!Promise} 
  */
 function deleteEntityFromPromise(entityPromise) {
   var deletePromise = 
@@ -110,116 +128,130 @@ function deleteEntityFromPromise(entityPromise) {
   return deletePromise;
 }
 
+/**
+ * Wrapper function for checkDeleted
+ * If an error is thrown (i.e., entity not deleted), it will be caught
+ * @param {!Promise=} entityPromise promise returned when entity was made
+ * @return {!Promise} 
+ */
 async function checkDeletedFromPromise(entityPromise) {
-  await sleep(DEFAULT_SLEEP);
+  await sleep(DEFAULT_SLEEP); // Allow the delete some time to propogate 
 
   const checkDeletedPromise = 
     entityPromise.then((promiseValue) => {
       checkDeleted(promiseValue.id)
       .catch((error) => console.log(error));
     });
+
+  return checkDeletedPromise;
 }
 
-function checkEntityMatchesFromPromise(entityPromise, expectedData) {
-  const checkMatchesPromise = 
+/**
+ * Wrapper for checkEntityEquals
+ * If the entity doesn't match expected data, catches the error
+ * @param {!Promise=} entityPromise promise returned when entity was made
+ * @param {json=} expectedData expected DB entry
+ * @return {!Promise}
+ */
+function checkEntityEqualsFromPromise(entityPromise, expectedData) {
+  const checkEqualsPromise = 
       entityPromise.then(promiseValue => {
-        checkEntityMatches(promiseValue.id, expectedData)
-        .catch((error) => console.log("Equality check failed", error));
+        checkEntityEquals(promiseValue.id, expectedData)
+        .catch((error) => console.log("Equality check FAILURE", error));
       });
   return entityPromise;
 }
 
 
-/**
- * Algolia helper functions
- */
+// Algolia helper functions 
 
+/**
+ * Retrieve an entity from Algolia as a JSON object if it exists
+ * @param {number=} objectID primary key, equal to entityId
+ * @return {!Promise}
+ */
 function getObjectFromAlgolia(objectID) {
-  //index.getObject(objectID).then(object => console.log(JSON.stringify(object.data))).catch((error) => console.log(error));
-  return index.getObject(objectID).catch((error) => console.log(error));
+  return index.getObject(objectID)
+              .catch((error) => {
+                console.log(`Couldn't retrieve ${objectID} because ${error}`)
+              });
 }
 
+/**
+ * Wrapper for getObjectFromAlgolia
+ * if the object is empty/null/doesn't exist etc, catches as an error
+ * @param {!Promise=} promise promise returned when entity was made in firebase
+ * @return {!Promise}
+ */
 function getObjectFromAlgoliaWithPromise(promise) {
   const algoliaPromise = promise.then(promiseValue => getObjectFromAlgolia(promiseValue.id));
   algoliaPromise.then(object => {
     if (typeof object !== 'undefined') {
       console.log(`Found ${JSON.stringify(object.data)}`);
-    } //else the error message will handle it. 
+    } else {
+      throw("Malformed object returned from algolia");
+    }
   }).catch((error) => console.log(error));
   
   return algoliaPromise; 
 }
 
+/**
+ * Checks that the algolia record was deleted 
+ * Assuming original entity deleted in firestore
+ * @param {!Promise=} promise promise returned when entity was made in firebase
+ * @return {!Promise}
+ */
 async function checkObjectDeletedFromAlgoliaWithPromise(promise) {
-  await sleep(DEFAULT_SLEEP*2);
-  getObjectFromAlgoliaWithPromise(promise).then(object => {
-    throw("Algolia object should not exist but it does")
-  }).catch(function onError(error) {console.log("Deleted from algolia. Above message normal.")});
-
+  await sleep(DEFAULT_SLEEP*2); // Wait for deletion to propogate
+  const algoliaPromise = 
+      getObjectFromAlgoliaWithPromise(promise).then(object => {
+        throw("Algolia object should not exist but it does")
+      }).catch(function onError(error) {
+        console.log("Above malformed message normal. Algolia deletion SUCCESS")
+      });
+  return algoliaPromise;
 }
-
+/**
+ * Gets the entity object from algolia, calls checkEntityEqualsFromPromise
+ * with the entityObject as the expectedData
+ * Catches error if they aren't equal
+ * @param {!Promise=} entityPromise promise returned when entity was made
+ */
 async function checkAlgoliaObjectEqualsFirestoreEntityFromPromise(entityPromise) {
 //  await sleep(DEFAULT_SLEEP) 
   const algoliaPromise = getObjectFromAlgoliaWithPromise(entityPromise);
   algoliaPromise.then(object => {
     const algoliaData = object.data;
-    checkEntityMatchesFromPromise(entityPromise, algoliaData);
+    checkEntityEqualsFromPromise(entityPromise, algoliaData);
   }).catch((error) => console.log("Weird error while comparing algolia and firebase ", error));
 }
-/**
- * Various helper functions
- */
+
+// Various helper functions
 
 /**
- * Throws an exception 
+ * Deletes entity from firestore, then checks it _really_ was deleted
+ * from firestore and algolia
+ * @param {!Promise=} initialPromise promise returned when entity was made
  */
-function throwPromiseNotFulfilled(querablePromise, action) {
-  if (querablePromise.isPending()) {
-    throw(`Tried accessing a still pending promise while ${action}`);
-  } else if (querablePromise.isRejected()) {
-    throw(`Tried accessing a rejected promise while ${action}`);
-  } else {
-    throw(`throwPromiseNotFulfilled accessed impromerly by ${action}`);
-  }
+function deleteFromEverywhere(initialPromise) {
+  setTimeout(function(){
+    console.log("Attemping to delete from everywhere"); 
+    const deletePromise = deleteEntityFromPromise(initialPromise);
+    const checkDeleted = checkDeletedFromPromise(initialPromise);
+    const checkDeletedFromAlgolia = checkObjectDeletedFromAlgoliaWithPromise(initialPromise)
+  }, 10000);
 }
 
 /**
- * Modifies a JS promise by adding isPending, isRejected, isFulfilled states
- * Basically modifies a promise into a promise that modifies state 
+ * Checks equivalence of JSON objects piecewise
+ * @param {json=} jsonObjectA initial json object
+ * @param {json=} jsonObjectB json object to compare against
  */
-function MakeQuerablePromise(promise) {
-    // Don't modify any promise that has been already modified.
-    if (promise.isResolved) return promise;
-
-    // Set initial state
-    var isPending = true;
-    var isRejected = false;
-    var isFulfilled = false;
-
-    // Observe the promise, saving the fulfillment in a closure scope.
-    var result = promise.then(
-        function(v) {
-            isFulfilled = true;
-            isPending = false;
-            return v; 
-        }, 
-        function(e) {
-            isRejected = true;
-            isPending = false;
-            throw e; 
-        }
-    );
-
-    result.isFulfilled = function() { return isFulfilled; };
-    result.isPending = function() { return isPending; };
-    result.isRejected = function() { return isRejected; };
-    return result;
-}
-
-function isEquivalent(a, b) {
+function isEquivalent(jsonObjectA, jsonObjectB) {
     // Create arrays of property names
-    var aProps = Object.getOwnPropertyNames(a);
-    var bProps = Object.getOwnPropertyNames(b);
+    var aProps = Object.getOwnPropertyNames(jsonObjectA);
+    var bProps = Object.getOwnPropertyNames(jsonObjectB);
 
     // If number of properties is different, objects are not equivalent
     if (aProps.length != bProps.length) {
@@ -230,7 +262,7 @@ function isEquivalent(a, b) {
         var propName = aProps[i];
 
         // If values of same property are not equal, objects are not equivalent
-        if (a[propName] !== b[propName]) {
+        if (jsonObjectA[propName] !== jsonObjectB[propName]) {
             return false;
         }
     }
@@ -239,26 +271,29 @@ function isEquivalent(a, b) {
     return true;
 }
 
+/**
+ * Helpful sleep function to delay functions from processing 
+ * while waiting for effects to propogate across DBs
+ * @param {number=} ms sleep for this many ms
+ * @return {!Promise}
+ */
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 
 
-/**
- * Make some functions available for import
- */
+
+// Make some functions available for import
 module.exports.addEntity = addEntity;
 module.exports.updateEntityFromPromise = updateEntityFromPromise;
-module.exports.deleteEntityFromPromise = deleteEntityFromPromise;
-module.exports.checkDeletedFromPromise = checkDeletedFromPromise;
 
-module.exports.sleep = sleep;
+//module.exports.checkEntityEqualsFromPromise = checkEntityEqualsFromPromise;
 
-module.exports.MakeQuerablePromise = MakeQuerablePromise;
-module.exports.checkEntityMatchesFromPromise = checkEntityMatchesFromPromise;
-
-module.exports.getObjectFromAlgoliaWithPromise = getObjectFromAlgoliaWithPromise;
+//module.exports.getObjectFromAlgoliaWithPromise = getObjectFromAlgoliaWithPromise;
 
 module.exports.checkObjectDeletedFromAlgoliaWithPromise = checkObjectDeletedFromAlgoliaWithPromise;
 module.exports.checkAlgoliaObjectEqualsFirestoreEntityFromPromise = checkAlgoliaObjectEqualsFirestoreEntityFromPromise;
+
+module.exports.deleteFromEverywhere = deleteFromEverywhere;
+module.exports.sleep = sleep;
