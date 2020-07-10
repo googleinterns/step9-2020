6ea3d5012c36981dd2ff23d8c5b6886932a7b4bc
Rob <robbie.marcus@gmail.com>
2020-07-10 03:12:05 +0000

    Explain what certain testing dependencies are doing (firebase, mocha, chai, sinon)

--- server/utils/firebase_algolia_server_code/functions/test/testConfig.js
+++ server/utils/firebase_algolia_server_code/functions/test/testConfig.js
@@ -6,9 +6,19 @@
  */
 
 /**
- * Initialize the firebase test mocking environment.
+ * Initialize the firebase test mocking environment. This will:
+ * - Take care of the appropriate setup and teardown for your tests
+ *   such as setting and unsetting environment variables needed 
+ *   by firebase-functions.
+ * - Generates sample data and event context, so that you only have 
+ *   to specify the fields that are relevant to your test.
+ * - Provide mock wrappers for event driven firebase functions.  
+ * - Read more here: `https://firebase.google.com/docs/functions/unit-testing`.
+ *
  * Having problems running this locally? 
- * - Download a firebase service key.
+ * - Download a firebase service key and upload to the `functions` directory
+ *   with filename `step9-2020-capstone.json`. 
+ * - That is all you need to do.   
  * - *DON'T* push this key to github please.  
  */
 const test = require('firebase-functions-test')({
@@ -17,7 +27,21 @@ const test = require('firebase-functions-test')({
   projectId: 'step9-2020-capstone',
 }, 'step9-2020-capstone.json');
 
-// Initialize chai, sinon, sinon-chai. 
+/**
+ * Initialize mocha, chai, and sinon. 
+ * Mocha is:  
+ * - feature rich node js test framework.
+ * - encapsulates test suites (describe-block), and test-cases (it-block).
+ * - provides test coverage.
+ * Chai is: 
+ * - An alternative to built in default equality/compare tests, which will
+ *   annoyingly pass tests even if their are errors being thrown : (
+ * - Chai does not have this problem.
+ * - Three assertion interfaces: expect, assert, should. 
+ * Sinon is: 
+ * - A utility to spy on external methods being used by systems under test.
+ * - Very handy, can do more but this is the primary purpose in these tests. 
+ */
 const assert = require('chai').assert;
 const chai = require("chai");
 const sinon = require("sinon");
