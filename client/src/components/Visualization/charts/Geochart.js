e88e9b5743093e3404b868a3ac5a7218bda4af97
kirakira0 <kiratoal0@gmail.com>
2020-07-13 18:36:28 +0000

    US Geochart with sample data

--- client/src/components/Visualization/charts/Geochart.js
+++ client/src/components/Visualization/charts/Geochart.js
@@ -1,10 +1,27 @@
+import { Chart } from "react-google-charts";
 import React from 'react';
 
 const Geochart = () => {
 
+  const data = [
+    ["State", "Random Number"],
+    ["Alaska", 301],
+    ["California", 200],
+    ["Texas", 180],
+    ["Wyoming", 28]
+  ];
+
+  const options = {
+    legend: 'none',     
+    resolution: 'provinces',
+    region:'US',
+    tooltip: {trigger:'none'}  
+  };
+
   return (
     <div className="search-header center">
       <p>Hello</p>
+      <Chart chartType="GeoChart" width="100%" height="400px" data={data} options={options}/>
     </div>
   );
 };
