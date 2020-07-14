d88d26d44c6ae9c7c03c948af12a5a5919858456
Rob <robbie.marcus@gmail.com>
2020-07-14 17:06:20 +0000

    Renamed functions -> firebaseFunctions

--- server/utils/cloudFunctions/functions/firebaseConfig.js
+++ server/utils/cloudFunctions/functions/firebaseConfig.js
@@ -6,14 +6,14 @@
  */
 
 // Initialize the firebase functions sdk. 
-const functions = require('firebase-functions');
+const firebaseFunctions = require('firebase-functions');
 
 // Access an individual document in our `ads` documents collection.
 const DEV_ADS_DOC_NAME = 'dev_ads/{adId}';
 const PROD_ADS_DOC_NAME = 'ads/{adId}';
 
 // Initialize access to the specific collections cloud functions watch over. 
-const DEV_ADS_DOCS = functions.firestore.document(DEV_ADS_DOC_NAME);
-const PROD_ADS_DOCS = functions.firestore.document(PROD_ADS_DOC_NAME);
+const DEV_ADS_DOCS = firebaseFunctions.firestore.document(DEV_ADS_DOC_NAME);
+const PROD_ADS_DOCS = firebaseFunctions.firestore.document(PROD_ADS_DOC_NAME);
 
-module.exports = { functions, DEV_ADS_DOCS, PROD_ADS_DOCS };
+module.exports = { firebaseFunctions, DEV_ADS_DOCS, PROD_ADS_DOCS };
