4600a8fb7c41bd70b8858c0ddc3b9600d13a7156
scarletnguyen <scarlet.nguyen01@gmail.com>
2020-07-17 12:39:46 -0400

    Setup DEV and PROD config for Algolia

--- client/src/constants/algolia_config.js
+++ client/src/constants/algolia_config.js
@@ -1,6 +1,6 @@
 import algoliasearch from 'algoliasearch/lite';
 
-const SEARCH_CLIENT = algoliasearch(
+const searchClient = algoliasearch(
   'E82P8D3AVW',
   '18f48284be85f748f3201280dc0938ce'
 );
@@ -8,4 +8,7 @@ const SEARCH_CLIENT = algoliasearch(
 const DEV_INDEX = 'dev_ADS';
 const PROD_INDEX = 'prod_ADS';
 
-export { SEARCH_CLIENT, DEV_INDEX, PROD_INDEX };
+const algoliaIndex =
+  process.env.NODE_ENV === 'development' ? DEV_INDEX : PROD_INDEX;
+
+export { searchClient, algoliaIndex };
