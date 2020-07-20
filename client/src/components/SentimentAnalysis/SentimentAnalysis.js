f1462b86f758a49410cc881e4017627e63c1f048
scarletnguyen <scarlet.nguyen01@gmail.com>
2020-07-20 19:33:05 -0400

    Add CAPCHA for SA

--- client/src/components/SentimentAnalysis/SentimentAnalysis.js
+++ client/src/components/SentimentAnalysis/SentimentAnalysis.js
@@ -1,9 +1,11 @@
 import './SentimentAnalysis.css';
 
 import { AnalysisInput, ColorBar, TermsDisplay } from './Helpers';
+import React, { useState } from 'react';
 
+import { CLIENT_KEY } from '../../constants/capcha_config';
 import { INPUT_LIST } from '../../constants/analysis_constants';
-import React from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
 import tardigrade from '../../images/tardigrade.png';
 
 const mockContentTerms = [
@@ -25,34 +27,46 @@ const mockHeaderTerms = [
   '{"name": "2020", "type": "NUMBER", "salience": 0.0}',
 ];
 
-const SentimentAnalysis = () => (
-  <div className="search-container">
-    <div className="search-header center">
-      <img src={tardigrade} className="logo" alt="logo" />
-      <div className="center">
-        {INPUT_LIST.map((input, index) => (
-          <AnalysisInput
-            key={index}
-            label={input.label}
-            placeholder={input.placeholder}
-          />
-        ))}
+const SentimentAnalysis = () => {
+  const [isVerified, setIsVerified] = useState(false);
+
+  const onChange = value => {
+    setIsVerified(value !== null);
+  };
+
+  return (
+    <div className="search-container">
+      <div className="search-header center bottom-padding">
+        <img src={tardigrade} className="logo" alt="logo" />
+        <div className="center">
+          {INPUT_LIST.map((input, index) => (
+            <AnalysisInput
+              key={index}
+              label={input.label}
+              placeholder={input.placeholder}
+            />
+          ))}
+        </div>
+        {isVerified ? (
+          <button className="analysis-button">Analyze</button>
+        ) : (
+          <ReCAPTCHA sitekey={CLIENT_KEY} onChange={onChange} />
+        )}
       </div>
-      <button className="analysis-button">Analyze</button>
+      <div className="analysis-result-container center">
+        <div className="analysis-card center">
+          <h4>Header Analysis</h4>
+          <ColorBar score={0.4} magnitude={0.7} />
+          <TermsDisplay termList={mockHeaderTerms} />
+        </div>
+        <div className="analysis-card center">
+          <h4>Content Analysis</h4>
+          <ColorBar score={-0.1} magnitude={0.2} />
+          <TermsDisplay termList={mockContentTerms} />
+        </div>
+      </div>
     </div>
-    <div className="analysis-result-container center">
-      <div className="analysis-card center">
-        <h4>Header Analysis</h4>
-        <ColorBar score={0.4} magnitude={0.7} />
-        <TermsDisplay termList={mockHeaderTerms} />
-      </div>
-      <div className="analysis-card center">
-        <h4>Content Analysis</h4>
-        <ColorBar score={-0.1} magnitude={0.2} />
-        <TermsDisplay termList={mockContentTerms} />
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
 export default SentimentAnalysis;
