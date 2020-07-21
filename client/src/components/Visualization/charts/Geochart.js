1914c4cbec5d84c0d9414282023a00829c6c202b
kirakira0 <32830420+kirakira0@users.noreply.github.com>
2020-07-21 13:09:59 -0700

    Update Geochart.js

--- client/src/components/Visualization/charts/Geochart.js
+++ client/src/components/Visualization/charts/Geochart.js
@@ -14,8 +14,8 @@ import { app, database } from '../../../firebase/firebase';
 const Geochart = () => {
 
   const [adTotal, setAdTotal] = useState(0);
-  const width = "700px";
-  const height = "400px";
+  const width = "700";
+  const height = "400";
 
   class GeochartAd {
     constructor(id) {
@@ -67,7 +67,7 @@ const Geochart = () => {
   return (
     <div className="search-header center">
       <p>Impressions Geochart</p>
-      <Chart chartType="GeoChart" width={ width } height={ height } data={ adTotal } options={ options } />
+      <Chart chartType="GeoChart" width={width} height={height} data={adTotal} options={options} />
     </div>
   );
 };
