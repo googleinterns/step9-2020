06679e9fe1d7f0c0718ffc8e8b07b7369166b15e
scarletnguyen <scarlet.nguyen01@gmail.com>
2020-07-21 15:28:44 -0400

    Address Andrew's comments

--- preprocess/analysis/handlers/routes.py
+++ preprocess/analysis/handlers/routes.py
@@ -1,11 +1,17 @@
 from flask import Flask, jsonify, request, abort
-from language_analysis import analyze_entities, analyze_sentiment
+# When run pytest, remove the `handlers.` initial to stop the error
+from handlers.language_analysis import analyze_entities, analyze_sentiment
 
-ERROR_MESSAGE = ('"header" or "content" field was either empty,' +
-                 'not included in form or has invalid values.')
 MAX_CHAR_LIMIT = 1000
 
-def is_valid(string):
+def is_valid(type, string):
+  if string is None:
+    abort(500, 'Incorrect "' + type + '" field name.')
+  if not string:
+    abort(500, '"' + type + '" field is empty.')
+  if len(string) > MAX_CHAR_LIMIT:
+    abort(500, 'String in "' + type + '" field exceeds 1000 char limit: ' + 
+          len(string) + ' chars.')
   return not (string is None or not string or len(string) > MAX_CHAR_LIMIT)
 
 def configure_routes(app):
@@ -15,15 +21,21 @@ def configure_routes(app):
 
   @app.route('/analysis', methods=['POST'])
   def analyze():
+    result_dict = dict()
     header = request.form.get('header')
     content = request.form.get('content')
 
-    if is_valid(header) and is_valid(content):
-      return jsonify(
-        header_sentiment=analyze_sentiment(header),
-        header_entities=analyze_entities(header),
-        content_sentiment=analyze_sentiment(content),
-        content_entities=analyze_entities(content),
-      )
-    else:
-      abort(500, ERROR_MESSAGE)  # 500 Internal Server Error server error
+    if is_valid('header', header):
+      header_sentiment = analyze_sentiment(header)
+      header_entities = analyze_entities(header)
+    
+    if is_valid('content', content):
+      content_sentiment = analyze_sentiment(content)
+      content_entities = analyze_entities(content)
+
+    return jsonify(
+      header_sentiment=header_sentiment, 
+      header_entities=header_entities, 
+      content_sentiment=content_sentiment, 
+      content_entities=content_entities
+    )
--- preprocess/analysis/handlers/test_routes.py
+++ preprocess/analysis/handlers/test_routes.py
@@ -5,13 +5,16 @@ Created on Tue Jul 20 16:36:57 2020
 
 @author: scarlet
 """
-from flask import Flask
-from routes import configure_routes, MAX_CHAR_LIMIT
-from test_language_analysis import STRING_ONE, STRING_TWO
 import pytest
+from flask import Flask
+from routes import MAX_CHAR_LIMIT, configure_routes
 
-analysis_url = '/analysis'
-form_type = 'multipart/form-data'
+HEADER = 'Fighting Climate Change | Pete Buttigieg 2020 | Learn More'
+CONTENT = ('Climate change is a life and death issue for our generation. ' +
+              'It\'s time for bold action.')
+
+URL = '/analysis'
+FORM_TYPE = 'multipart/form-data'
 
 @pytest.fixture
 def client():
@@ -21,41 +24,41 @@ def client():
   return client
 
 def test_analysis_route_success(client):
-  mock_data = { 'header': STRING_ONE, 'content': STRING_TWO }
-  response = client.post(analysis_url, content_type=form_type, data=mock_data)
+  mock_data = { 'header': HEADER, 'content': CONTENT }
+  response = client.post(URL, content_type=FORM_TYPE, data=mock_data)
   assert response.status_code == 200
 
 def test_analysis_route_fail_missing_field(client):
   # missing "content" field
-  mock_data = { 'header': STRING_ONE }
-  response = client.post(analysis_url, content_type=form_type, data=mock_data)
+  mock_data = { 'header': HEADER }
+  response = client.post(URL, content_type=FORM_TYPE, data=mock_data)
   assert response.status_code == 500
 
 def test_analysis_route_fail_invalid_fields(client):
-  # mispelled field names 
-  mock_data = { 'headers': STRING_ONE, 'contents': STRING_TWO }
-  response = client.post(analysis_url, content_type=form_type, data=mock_data)
+  # incorrect field names 
+  mock_data = { 'title': HEADER, 'body': CONTENT }
+  response = client.post(URL, content_type=FORM_TYPE, data=mock_data)
   assert response.status_code == 500
 
 def test_analysis_route_fail_empty_fields(client):
   # empty fields 
   mock_data_1 = { 'header': '', 'content': '' }
-  response = client.post(analysis_url, content_type=form_type, data=mock_data_1)
+  response = client.post(URL, content_type=FORM_TYPE, data=mock_data_1)
   assert response.status_code == 500
 
    # empty a field
-  mock_data_2 = { 'header': STRING_ONE, 'content': '',}
-  response = client.post(analysis_url, content_type=form_type, data=mock_data_2)
+  mock_data_2 = { 'header': HEADER, 'content': '',}
+  response = client.post(URL, content_type=FORM_TYPE, data=mock_data_2)
   assert response.status_code == 500
 
 def test_analysis_route_fail_exceed_character_limit(client):
   exceeded_string = "x" * (MAX_CHAR_LIMIT + 1)
-  mock_data = { 'header': STRING_ONE, 'content': exceeded_string }
-  response = client.post(analysis_url, content_type=form_type, data=mock_data)
+  mock_data = { 'header': HEADER, 'content': exceeded_string }
+  response = client.post(URL, content_type=FORM_TYPE, data=mock_data)
   assert response.status_code == 500
 
 def test_analysis_route_success_within_character_limit_boundary(client):
   inbound_string = "x" * MAX_CHAR_LIMIT
-  mock_data = { 'header': STRING_ONE, 'content': inbound_string }
-  response = client.post(analysis_url, content_type=form_type, data=mock_data)
+  mock_data = { 'header': HEADER, 'content': inbound_string }
+  response = client.post(URL, content_type=FORM_TYPE, data=mock_data)
   assert response.status_code == 200
