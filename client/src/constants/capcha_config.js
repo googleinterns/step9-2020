780a057f2a281a67a8ac9bb6f429ba2c8a0a95ed
scarletnguyen13 <scarlet.nguyen01@gmail.com>
2020-07-21 23:58:54 +0000

    Fix Capcha api key

--- client/src/constants/capcha_config.js
+++ client/src/constants/capcha_config.js
@@ -1,5 +1,5 @@
 const DEV_KEY = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI';
-const PROD_KEY = '6LdY27MZAAAAAOl3M0GEr7b3NeKyU0pRVNpW4UsR';
+const PROD_KEY = '6Lf6V7QZAAAAAKU5Pkol0KkOELbo8H_rdNiAa1Xq';
 
 const CLIENT_KEY = process.env.NODE_ENV === 'development' ? DEV_KEY : PROD_KEY;
 
