f08958103f88d05a18e149df661725662b1abaf3
scarletnguyen <scarlet.nguyen01@gmail.com>
2020-07-22 21:52:06 -0400

    Remove unnecessary imports

--- client/src/components/Visualization/charts/Geochart.js
+++ client/src/components/Visualization/charts/Geochart.js
@@ -4,24 +4,22 @@
  * Date: 2020/07/13
  */
 
-import { ads } from '../../../firebase/FirestoreDocumentReader';
-import { Chart } from "react-google-charts";
-import firebase from '../../../firebase/firebase';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import firebase, { app, database } from '../../../firebase/firebase';
+
+import { Chart } from 'react-google-charts';
 import { states } from './StateDataParser';
-import { app, database } from '../../../firebase/firebase';
 
 const Geochart = () => {
-
   const [adTotal, setAdTotal] = useState(0);
-  const WIDTH = "700";
-  const HEIGHT = "400";
+  const WIDTH = '700';
+  const HEIGHT = '400';
 
   class GeochartAd {
     constructor(id) {
-    /**
-    * @param {string} id
-    */
+      /**
+       * @param {string} id
+       */
       this.id = id;
     }
     toString() {
@@ -31,41 +29,48 @@ const Geochart = () => {
 
   // Firestore data converter converts snapshots to custom objects.
   const adConverter = {
-    toFirestore: function(ad) {
+    toFirestore(ad) {
       return {
         id: ad.id,
-      }
+      };
     },
-    fromFirestore: function(snapshot, options){
+    fromFirestore(snapshot, options) {
       const data = snapshot.data(options);
-      return new GeochartAd(data.id)
-    }
-  }
-    
+      return new GeochartAd(data.id);
+    },
+  };
+
   useEffect(async () => {
-    let data = [["State", "Total Number of Ads"]];
-    for (let state in states) {
+    const data = [['State', 'Total Number of Ads']];
+    for (const state in states) {
       const documentRef = database.collection('ads');
-      const query = await documentRef.where("geoTarget", "array-contains", state)
-                                  .withConverter(adConverter)
-                                  .get();
+      const query = await documentRef
+        .where('geoTarget', 'array-contains', state)
+        .withConverter(adConverter)
+        .get();
       data.push([state, query.docs.length]); // Update data table.
     }
-    setAdTotal(data)
-  }, [])
+    setAdTotal(data);
+  }, []);
 
   const options = {
     enableRegionInteractivity: true,
-    legend: {textStyle: {color: 'black', fontSize: 10}},     
+    legend: { textStyle: { color: 'black', fontSize: 10 } },
     resolution: 'provinces',
-    region:'US',
-    tooltip: {trigger:'focus'} // Trigger info box on mouse hover over state.
+    region: 'US',
+    tooltip: { trigger: 'focus' }, // Trigger info box on mouse hover over state.
   };
 
   return (
     <div className="search-header center">
       <p>Impressions Geochart</p>
-      <Chart chartType="GeoChart" width={WIDTH} height={HEIGHT} data={adTotal} options={options} />
+      <Chart
+        chartType="GeoChart"
+        width={WIDTH}
+        height={HEIGHT}
+        data={adTotal}
+        options={options}
+      />
     </div>
   );
 };
