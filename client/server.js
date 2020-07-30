169c2b248377ea5bb777f2411798ee3323eafe73
scarletnguyen <scarlet.nguyen01@gmail.com>
2020-07-30 13:06:56 -0400

    Fix react router cannot GET from reloading

--- client/server.js
+++ client/server.js
@@ -4,8 +4,12 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'), err => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
 });
 
 app.listen(8080);
